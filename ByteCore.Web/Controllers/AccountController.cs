9acbd90c87f79bb3942ae272eb75a1da0d6cdec1
tarakanav <sdwcktarakanov@gmail.com>
2025-02-17 20:08:55 +0200

    Add project files.

---
+++ ByteCore.Web/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ByteCore.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Registration
+        public ActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
