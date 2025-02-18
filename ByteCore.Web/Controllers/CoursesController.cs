7ab59c3ab4907208d4e7d378ecaa238ab92a9494
TimaDyakonu <tima_dyakonu@mail.ru>
2025-02-18 15:23:44 +0200

    Add course roadmap page

--- ByteCore.Web/Controllers/CoursesController.cs
+++ ByteCore.Web/Controllers/CoursesController.cs
@@ -40,5 +40,73 @@ namespace ByteCore.Web.Controllers
 
             return View(course);
         }
+        // GET: /Courses/{id}/Roadmap
+        [Route("{id:int}/Roadmap")]
+        public ActionResult Roadmap(int id)
+        {
+            var course = GetCourseById(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+
+            var roadmap = new CourseRoadmapModel
+            {
+                Course = course,
+                Steps = new List<RoadmapStep>
+                {
+                    new RoadmapStep
+                    {
+                        StepTitle = "Introduction",
+                        Description = "Overview of the course",
+                        Week = "1"
+                    },
+                    new RoadmapStep
+                    {
+                        StepTitle = "Fundamentals",
+                        Description = "Basic concepts and principles",
+                        Week = "2"
+                    },
+                    new RoadmapStep
+                    {
+                        StepTitle = "Advanced Topics",
+                        Description = "Deep dive into complex subjects",
+                        Week = "3"
+                    }
+                }
+            };
+
+            ViewBag.CourseTitle = course.Title;
+            return View(roadmap);
+        }
+
+        // Пример метода для получения курса
+        private CourseModel GetCourseById(int id)
+        {
+            return new CourseModel
+            {
+                Id = id,
+                Title = "Sample Course",
+                ShortDescription = "This is a sample course description",
+                Description = "Detailed course description here...",
+                Instructor = "John Doe",
+                Duration = "4 weeks",
+                StartDate = new DateTime(2025, 3, 1),
+                ImageUrl = "/images/sample-course.jpg"
+            };
+        }
+
+        // Пример метода для получения шагов roadmap
+        private List<string> GetRoadmapSteps(int courseId)
+        {
+            return new List<string>
+            {
+                "Introduction to the Course",
+                "Module 1: Basics",
+                "Module 2: Intermediate Concepts",
+                "Module 3: Advanced Topics",
+                "Final Project"
+            };
+        }
     }
 }
---
+++ ByteCore.Web/Models/CourseRoadmapModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ByteCore.Web.Models
+{
+    public class CourseRoadmapModel
+    {
+        public CourseModel Course { get; set; }
+        public List<RoadmapStep> Steps { get; set; }
+    }
+
+    public class RoadmapStep
+    {
+        public string StepTitle { get; set; }
+        public string Description { get; set; }
+        public string Week { get; set; }
+    }
+}
