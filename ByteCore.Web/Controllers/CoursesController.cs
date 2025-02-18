7a3beb8fbdba90bb06cace33b6b995b841163043
tarakanav <sdwcktarakanov@gmail.com>
2025-02-18 13:48:24 +0200

    Add course model and details view with routing updates

--- ByteCore.Web/Controllers/CoursesController.cs
+++ ByteCore.Web/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteCore.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,7 @@ using System.Web.Mvc;
 
 namespace ByteCore.Web.Controllers
 {
+    [RoutePrefix("Courses")]
     public class CoursesController : Controller
     {
         // GET: Courses
@@ -18,6 +20,25 @@ namespace ByteCore.Web.Controllers
         public ActionResult Overview()
         {
             return View();
+        }
+
+        // GET: Courses/1
+        [Route("{id:int}")]
+        public ActionResult Course(int id)
+        {
+            var course = new Course
+            {
+                Id = id,
+                Title = "Introduction to Web Development",
+                ShortDescription = "Learn the fundamentals of HTML, CSS, and JavaScript.",
+                Description = "This course covers the basics of web development including responsive design, front-end frameworks, and modern JavaScript techniques. You'll work on hands-on projects and gain real-world skills.",
+                Instructor = "Jane Doe",
+                Duration = "6 weeks",
+                StartDate = DateTime.Now.AddDays(14),
+                ImageUrl = "https://dummyimage.com/600x400/343a40/6c757d"
+            };
+
+            return View(course);
         }
     }
 }
---
+++ ByteCore.Web/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ByteCore.Web.Models
+{
+	public class Course
+	{
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public string Instructor { get; set; }
+        public string Duration { get; set; }
+        public DateTime StartDate { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
