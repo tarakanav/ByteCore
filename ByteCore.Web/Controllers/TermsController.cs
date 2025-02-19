2386fd04cb3857eaa648793ca5026a123456da10
tarakanav <sdwcktarakanov@gmail.com>
2025-02-19 15:16:37 +0200

    Add Terms & Conditions page

---
+++ ByteCore.Web/Controllers/TermsController.cs
@@ -0,0 +1,13 @@
+using System.Web.Mvc;
+
+namespace ByteCore.Web.Controllers
+{
+    public class TermsController : Controller
+    {
+        // GET: /Terms/
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
