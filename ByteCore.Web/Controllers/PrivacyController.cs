005933770acddef624809c3edc7a439370398be6
tarakanav <sdwcktarakanov@gmail.com>
2025-02-19 15:23:35 +0200

    Add Privacy Policy page

---
+++ ByteCore.Web/Controllers/PrivacyController.cs
@@ -0,0 +1,13 @@
+using System.Web.Mvc;
+
+namespace ByteCore.Web.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        // GET: /Privacy/
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
