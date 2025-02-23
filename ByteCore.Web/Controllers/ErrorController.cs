34d399218c13b16cbd1499a1f4e9b0514d7cc47e
TimaDyakonu <tima_dyakonu@mail.ru>
2025-02-23 11:59:52 +0200

    add new page not found

---
+++ ByteCore.Web/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ByteCore.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error/NotFound
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
