f1ec0efda29a4572d72df3bf8f2d3095e9ff1928
tarakanav <sadwicktarakanov@gmail.com>
2025-02-23 19:30:06 +0200

    Implement custom error handling for 404 errors and update web.config

--- ByteCore.Web/Controllers/ErrorController.cs
+++ ByteCore.Web/Controllers/ErrorController.cs
@@ -4,9 +4,11 @@ namespace ByteCore.Web.Controllers
 {
     public class ErrorController : Controller
     {
-        // GET: Error/NotFound
+        // GET: Error/404
+        [Route("Error/404")]
         public ActionResult NotFound()
         {
+            Response.StatusCode = 404;
             return View();
         }
     }
