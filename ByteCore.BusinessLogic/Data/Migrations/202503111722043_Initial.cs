4769cdfd52aa4f8859b4b53e04c2a27acabda1d2
TimaDyakonu <tima_dyakonu@mail.ru>
2025-03-26 16:30:05 +0200

    Moved migrations to the BL layer

---
+++ ByteCore.BusinessLogic/Data/Migrations/202503111722043_Initial.cs
@@ -0,0 +1,75 @@
+﻿namespace ByteCore.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CourseModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        ShortDescription = c.String(),
+                        Description = c.String(),
+                        Instructor = c.String(),
+                        Duration = c.String(),
+                        StartDate = c.DateTime(nullable: false),
+                        ImageUrl = c.String(),
+                        UserModel_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.UserModels", t => t.UserModel_Id)
+                .Index(t => t.UserModel_Id);
+            
+            CreateTable(
+                "dbo.QuizModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        RewardPoints = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.QuestionModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        QuestionText = c.String(),
+                        CorrectOptionIndex = c.Int(nullable: false),
+                        QuizModel_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.QuizModels", t => t.QuizModel_Id)
+                .Index(t => t.QuizModel_Id);
+            
+            CreateTable(
+                "dbo.UserModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.CourseModels", "UserModel_Id", "dbo.UserModels");
+            DropForeignKey("dbo.QuestionModels", "QuizModel_Id", "dbo.QuizModels");
+            DropIndex("dbo.QuestionModels", new[] { "QuizModel_Id" });
+            DropIndex("dbo.CourseModels", new[] { "UserModel_Id" });
+            DropTable("dbo.UserModels");
+            DropTable("dbo.QuestionModels");
+            DropTable("dbo.QuizModels");
+            DropTable("dbo.CourseModels");
+        }
+    }
+}
---
+++ ByteCore.BusinessLogic/Data/Migrations/202503261340427_Renamed-Entities.cs
@@ -0,0 +1,56 @@
+﻿namespace ByteCore.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RenamedEntities : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.CourseModels", newName: "Courses");
+            RenameTable(name: "dbo.ChapterModels", newName: "Chapters");
+            RenameTable(name: "dbo.SectionModels", newName: "Sections");
+            RenameTable(name: "dbo.QuizModels", newName: "Quizs");
+            RenameTable(name: "dbo.QuestionModels", newName: "Questions");
+            RenameTable(name: "dbo.QuestionOptionModels", newName: "QuestionOptions");
+            RenameTable(name: "dbo.UserModels", newName: "Users");
+            RenameTable(name: "dbo.QuizResultAnswerModels", newName: "QuizResultAnswers");
+            RenameTable(name: "dbo.QuizResultModels", newName: "QuizResults");
+            RenameTable(name: "dbo.UserModelCourseModels", newName: "UserCourses");
+            RenameColumn(table: "dbo.UserCourses", name: "UserModel_Id", newName: "User_Id");
+            RenameColumn(table: "dbo.UserCourses", name: "CourseModel_Id", newName: "Course_Id");
+            RenameColumn(table: "dbo.Questions", name: "QuizModel_Id", newName: "Quiz_Id");
+            RenameColumn(table: "dbo.QuestionOptions", name: "QuestionModel_Id", newName: "Question_Id");
+            RenameColumn(table: "dbo.QuizResultAnswers", name: "QuizResultModel_Id", newName: "QuizResult_Id");
+            RenameIndex(table: "dbo.Questions", name: "IX_QuizModel_Id", newName: "IX_Quiz_Id");
+            RenameIndex(table: "dbo.QuestionOptions", name: "IX_QuestionModel_Id", newName: "IX_Question_Id");
+            RenameIndex(table: "dbo.QuizResultAnswers", name: "IX_QuizResultModel_Id", newName: "IX_QuizResult_Id");
+            RenameIndex(table: "dbo.UserCourses", name: "IX_UserModel_Id", newName: "IX_User_Id");
+            RenameIndex(table: "dbo.UserCourses", name: "IX_CourseModel_Id", newName: "IX_Course_Id");
+        }
+        
+        public override void Down()
+        {
+            RenameIndex(table: "dbo.UserCourses", name: "IX_Course_Id", newName: "IX_CourseModel_Id");
+            RenameIndex(table: "dbo.UserCourses", name: "IX_User_Id", newName: "IX_UserModel_Id");
+            RenameIndex(table: "dbo.QuizResultAnswers", name: "IX_QuizResult_Id", newName: "IX_QuizResultModel_Id");
+            RenameIndex(table: "dbo.QuestionOptions", name: "IX_Question_Id", newName: "IX_QuestionModel_Id");
+            RenameIndex(table: "dbo.Questions", name: "IX_Quiz_Id", newName: "IX_QuizModel_Id");
+            RenameColumn(table: "dbo.QuizResultAnswers", name: "QuizResult_Id", newName: "QuizResultModel_Id");
+            RenameColumn(table: "dbo.QuestionOptions", name: "Question_Id", newName: "QuestionModel_Id");
+            RenameColumn(table: "dbo.Questions", name: "Quiz_Id", newName: "QuizModel_Id");
+            RenameColumn(table: "dbo.UserCourses", name: "Course_Id", newName: "CourseModel_Id");
+            RenameColumn(table: "dbo.UserCourses", name: "User_Id", newName: "UserModel_Id");
+            RenameTable(name: "dbo.UserCourses", newName: "UserModelCourseModels");
+            RenameTable(name: "dbo.QuizResults", newName: "QuizResultModels");
+            RenameTable(name: "dbo.QuizResultAnswers", newName: "QuizResultAnswerModels");
+            RenameTable(name: "dbo.Users", newName: "UserModels");
+            RenameTable(name: "dbo.QuestionOptions", newName: "QuestionOptionModels");
+            RenameTable(name: "dbo.Questions", newName: "QuestionModels");
+            RenameTable(name: "dbo.Quizs", newName: "QuizModels");
+            RenameTable(name: "dbo.Sections", newName: "SectionModels");
+            RenameTable(name: "dbo.Chapters", newName: "ChapterModels");
+            RenameTable(name: "dbo.Courses", newName: "CourseModels");
+        }
+    }
+}
